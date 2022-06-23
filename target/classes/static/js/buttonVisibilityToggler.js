3ff8cb8dfbbf1e5d98e7e768d73657d1a5dda06a
Kaine <77235839+Beatoriche-sama@users.noreply.github.com>
2022-06-23 16:59:41 +0300

    Delete target directory

--- target/classes/static/js/buttonVisibilityToggler.js
+++
@@ -1,7 +0,0 @@
-function setCharacterInfoVisible(buttonSource){
-       var panel = buttonSource.next('.characterInfoPanel');
-       var isHidden = JSON.parse(buttonSource.val());
-       buttonSource.text(isHidden ? 'Show' : 'Hide');
-       buttonSource.val(!isHidden);
-       panel.toggle();
-    }
