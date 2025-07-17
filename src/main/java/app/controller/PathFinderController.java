983890a047f4d4902d9b4730fe8378cef06dff7c
VictoriaTaiwan <77235839+VictoriaTaiwan@users.noreply.github.com>
2025-07-17 14:09:38 +0800

    - Minor optimizations.

--- src/main/java/app/controller/PathFinderController.java
+++ src/main/java/app/controller/PathFinderController.java
@@ -3,15 +3,16 @@ package app.controller;
 import app.dto.Area;
 import app.dto.Coordinate;
 import app.repository.AreaRepository;
-
 import app.service.JsonConverter;
 import app.service.LogsParser;
 import app.service.Tsp;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.LinkedList;
